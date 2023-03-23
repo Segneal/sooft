3f30356fedf2bacec6b83d0c30e2b6cbb7b2e9bf
Segneal <znicolasheredia@gmail.com>
2023-03-22 22:14:07 -0300

    Update feriadosColores.ts

--- front/src/utils/feriadosColores.ts
+++ front/src/utils/feriadosColores.ts
@@ -4,14 +4,14 @@ const feriadosColores = ["Inamovible", "Trasladable", "Puente"];
 
 const getColorFromArray = (date: Date, feriados: Feriado[]) => {
   const d = new Date(date);
-  let holidays = feriados.filter((feriado) => {
+  let holidays = feriados?.filter((feriado) => {
     const d2 = new Date(feriado.fecha);
     return d2.getTime() === d.getTime();
   });
   if (holidays.length === 0) {
     return "";
   }
-  let colors = feriadosColores.filter(
+  let colors = feriadosColores?.filter(
     (feriado) => feriado.toLowerCase() === holidays[0].tipo.toLowerCase()
   );
 
